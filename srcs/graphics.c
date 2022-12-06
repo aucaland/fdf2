4607686af6f185c0e94ef7c5410a42d6a0340210
Aurelien Calandrino <aucaland@z3r6p1.42lyon.fr>
2022-12-06 13:43:47 +0100

    save rota begin

--- srcs/graphics.c
+++ srcs/graphics.c
@@ -6,7 +6,7 @@
 /*   By: aucaland <aucaland@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 16:35:19 by aurel             #+#    #+#             */
-/*   Updated: 2022/12/06 13:05:04 by aucaland         ###   ########.fr       */
+/*   Updated: 2022/12/06 13:40:05 by aucaland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_point	proj(t_fdf *fdf)
 {
 	fdf->point.x *= fdf->cam->scale;
 	fdf->point.y *= fdf->cam->scale;
-	fdf->point.z *= ((fdf->map->max_coeff) / (fdf->map->height * 0.2));
+	fdf->point.z *= ((fdf->map->max_coeff) * (fdf->map->height * 0.1));
 	fdf->data->color =  BLUE_F * 0.4 + RED_F * fdf->point.z * 0.4 + WHITE * 0.4;
 	fdf->point.x -= (fdf->map->width * fdf->cam->scale) / 2;
 	fdf->point.y -= (fdf->map->height * fdf->cam->scale) / 2;
@@ -35,8 +35,8 @@ void isometric(float *x, float *y, int *z, t_fdf *fdf)
 	(void)fdf;
 	*x = (*x - *y) * cos(0.5555);
 
-	*y = (*x + *y) * sin(0.5555) - *z * (cos(0.5555) - sin(0.5555)) * (fdf->cam->scale / fdf->map->height);
-	*z = cos(0.5555) - sin(0.5555);
+	*y = (*x + *y) * sin(0.5555) - *z  ; //(cos(0.5555) - sin(0.5555)) * (fdf->cam->scale  / (fdf->map->height_win / fdf->map->height))
+	//*z = cos(0.5555) - sin(0.5555);
 }
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
